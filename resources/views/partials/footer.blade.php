8cd79eee14d82fa8670fa2828ba02d9caf923c02
MichelepioBombai <michelebombai@icloud.com>
2023-04-14 09:57:37 +0200

    add-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,103 @@
-<footer class="bg-dark mt-auto">
-    dddddddddd
+<footer>
+    <section class="upper-footer text-bg-primary" style="height: 110px;">
+        <div class="container d-flex justify-content-center ">
+            <div class="d-flex me-5 my-4 ">
+                <img src="buy-comics-digital-comics.png" height="60px">
+                <p class="my-3 ms-2">DIGITAL COMICS</p>
+            </div>
+
+            <div class="d-flex me-5  my-4 ">
+                <img src="buy-comics-merchandise.png" height="60px">
+                <p class="my-3 ms-2">DC MERCHANDISE</p>
+            </div>
+
+            <div class="d-flex me-5 my-4">
+                <img src="buy-comics-shop-locator.png" height="60px">
+                <p class="my-3 ms-2">SUBSCRIPTION</p>
+            </div>
+
+            <div class="d-flex me-5 my-4">
+                <img src="buy-comics-subscriptions.png" height="60px">
+                <p class="my-3 ms-2">COMIC SHOP LOCATION</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="bottom-footer" style="background-image: url(footer-bg.jpg); height: 480px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 text-white d-flex flex-wrap my-5">
+
+                    <h3>DC COMICS</h3>
+                    <ul class="ps-0" style="list-style-type: none; position:relative; top: 50px; right: 150;">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+
+                    <h3>DC</h3>
+                    <ul class="ps-0" style="list-style-type: none; position:relative; top: 50px; right: 35;">
+                        <li>Terms of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+
+                    <h3>SITES</h3>
+                    <ul class="ps-0" style="list-style-type: none; position:relative; top: 50px; right: 65;">
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+
+
+                    </ul>
+
+                    <h3>SHOP</h3>
+                    <ul class="ps-0" style="list-style-type: none; position:relative; top: 50px; right: 70;">
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+
+                </div>
+
+                <div class="col-6">
+                    <img src="dc-logo-bg.png" width="500px" height="480">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sign-up" style="background-color: hsl(0deg 0% 18.82%);">
+        <div class="container d-flex justify-content-between">
+            <div class="my-5">
+                <button class="btn text-white" style="border-color: #0d6efd;">SIGN-UP NOW!</button>
+            </div>
+
+            <div class="my-5">
+                <p class="text-primary fs-4 d-inline">FOLLOW US</p>
+                <img class="ms-3" src="footer-facebook.png">
+                <img class="ms-3" src="footer-periscope.png">
+                <img class="ms-3" src="footer-pinterest.png">
+                <img class="ms-3" src="footer-twitter.png">
+                <img class="ms-3" src="footer-youtube.png">
+
+            </div>
+
+        </div>
+
+    </section>
+
 </footer>
